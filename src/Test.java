886d2e35f93e30c0ced46a21a55425ee212ad3bf
YuhengTian <yuheng.tian@cityfruit.io>
2019-09-17 15:42:34 +0800

    second commit

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int num = 1; num <= 5; num++) {
+            count += num;
+            System.out.println(count);
+        }
+    }
+}
