28df1c2c2fabaf5eb8b0afe805429d57d12cef20
YuhengTian <yuheng.tian@cityfruit.io>
2019-09-17 17:22:25 +0800

    v0.0.3

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         Counter sc = new Counter();
-        int count = 0;
         for (int num = 1; num <= 5; num++) {
             sc.count(num);
             sc.show();
